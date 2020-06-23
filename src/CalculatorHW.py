c88cd54f7577515764998c61d68238ddb69f2925
kb8njit <kb8@njit.edu>
2020-06-23 17:18:09 -0400

    Task: Updated CalculatorHW.py to include math functions and Calculator class

---
+++ src/CalculatorHW.py
@@ -0,0 +1,28 @@
+import math
+
+def add(x, y):
+    return float(x + y)
+
+def subtract(x, y):
+    return float(x - y)
+
+def multiply(x, y):
+    return float(x * y)
+
+def divide(x, y):
+    return float(x / y)
+
+def square(x):
+    return float(x * x)
+
+def sqrt(x):
+    return float(math.sqrt(x))
+
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
