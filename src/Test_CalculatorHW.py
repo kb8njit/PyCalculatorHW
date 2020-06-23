b8a2a0df744935ae3354daddb8b291ec619b2ebd
kb8njit <kb8@njit.edu>
2020-06-23 19:10:18 -0400

    Feature: Added Square.csv Calc Test in Test_CalculatorHW.py Task: Removed Division test

--- src/Test_CalculatorHW.py
+++ src/Test_CalculatorHW.py
@@ -3,6 +3,7 @@ import math
 from CalculatorHW import Calculator
 from CSV_Reader import CSVReader
 
+
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -10,7 +11,6 @@ class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
-
 
     def test_addition(self):
         self.assertEqual(self.calculator.add(2.2, 2.2), 4.4)
@@ -21,7 +21,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, int(row['Result']))
         test_data.clear()
 
-
     def test_subtraction(self):
         self.assertEqual(self.calculator.subtract(4.4, 2.2), 2.2)
         self.assertEqual(self.calculator.result, 2.2)
@@ -30,7 +29,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
         test_data.clear()
-
 
     def test_multiply(self):
         self.assertEqual(self.calculator.multiply(2.1, 2.1), 4.41)
@@ -41,24 +39,23 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, float(row['Result']))
         test_data.clear()
 
-
     def test_division(self):
         self.assertEqual(self.calculator.divide(6.0, 2.0), 3.0)
         self.assertEqual(self.calculator.result, 3.0)
-        test_data = CSVReader('/src/CSV_files/Division.csv').data
-        for row in test_data:
-            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
-            self.assertEqual(self.calculator.result, round(float(row['Result']), 9))
-        test_data.clear()
-
 
     def test_square(self):
         self.assertEqual(self.calculator.square(2.2), 4.84)
         self.assertEqual(self.calculator.result, 4.84)
+        test_data = CSVReader('/src/CSV_files/Square.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.square(row['Value 1']), float(row['Result']))
+            self.assertEqual(self.calculator.result, round(float(row['Result']), 9))
+        test_data.clear()
 
     def test_sqrt(self):
         self.assertEqual(self.calculator.sqrt(8.8), float(math.sqrt(8.8)))
         self.assertEqual(self.calculator.result, float(math.sqrt(8.8)))
+
 
 if __name__ == '__main__':
     unittest.main()
